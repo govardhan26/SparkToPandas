f8898cae0aeee85b2d97b9f53ecd318f58fd544f
govardhan26 <govardhan_selvaraj@yahoo.com>
2022-12-20 18:43:48 +0530

    version changes

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -7,6 +7,7 @@ import sys
 # sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
 
 project = 'SparkToPandas'
 copyright = '2022, Govardhan Selvaraj'
